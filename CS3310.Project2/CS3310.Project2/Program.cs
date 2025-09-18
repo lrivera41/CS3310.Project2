20917a67161610109a145e2e95a5b19a66546cd5
lrivera41 <lriv4157@gmail.com>
2025-09-17 20:45:41 -0600

    .

--- CS3310.Project2/CS3310.Project2/Program.cs
+++ CS3310.Project2/CS3310.Project2/Program.cs
@@ -3,31 +3,57 @@
 public class Program
 {
     private static int inversions;
+    private static readonly string RootDirectory = @"C:\Users\luismi\Documents\source\School\CS3310.Project2\CS3310.Project2\CS3310.Project2\CS3310.Project2";
     public static void Main(string[] args)
     {
         inversions = 0;
         var array = new int[] { 3, 1, 2 };
-        Sort(array);
-        Console.WriteLine($"Sorted array: {string.Join(", ", array)}");
+        CountInverse(array);
+        Console.WriteLine($"Array: {string.Join(", ", array)}");
         Console.WriteLine($"Inversions {inversions}{Environment.NewLine}");
-        
+
         inversions = 0;
         array = new int[] { 8, 7, 6, 5, 4, 3, 2, 1 };
-        Sort(array);
-        Console.WriteLine($"Sorted array: {string.Join(", ", array)}");
+        CountInverse(array);
+        Console.WriteLine($"Array: {string.Join(", ", array)}");
+        Console.WriteLine($"Inversions {inversions}{Environment.NewLine}");
+
+        inversions = 0;
+        array = ExtractFile("problem3.5test.txt");
+        CountInverse(array);
+        Console.WriteLine($"Array: {string.Join(", ", array ?? [])}");
+        Console.WriteLine($"Inversions {inversions}{Environment.NewLine}");
+
+        inversions = 0;
+        array = ExtractFile("problem3.5.txt");
+        CountInverse(array);
+        Console.WriteLine($"Array: {string.Join(", ", array ?? [])}");
         Console.WriteLine($"Inversions {inversions}{Environment.NewLine}");
     }
 
-    private static void Sort(int[] array)
+    private static int[]? ExtractFile(string fileName)
     {
+        int[] array = Array.Empty<int>();
+        var filePath = Path.Combine(RootDirectory, fileName);
+        if (!File.Exists(filePath)) return null;
+        var list = File.ReadLines(filePath)
+                       .Where(line => !string.IsNullOrWhiteSpace(line))
+                       .Select(line => int.Parse(line.Trim()))
+                       .ToList();
+
+        array = list.ToArray();
+
+        return array;
+    }
+
+    private static void CountInverse(int[]? array)
+    {
+        if (array is null) return;
         for (int i = 0; i < array.Length - 1; i++)
         {
-            if (array[i] > array[i + 1])
+            for (int j = i + 1; j < array.Length; j++)
             {
-                var temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                inversions++; 
+                if (array[i] > array[j]) inversions++;
             }
         }
     }
